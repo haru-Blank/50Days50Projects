e572575d3311d9b2a7a137535f18e28004736210
Harendra Bhujel <avash.nekros274@gmail.com>
2021-08-17 09:25:57 +0545

    showcase project

--- script.js
+++ script.js
@@ -2,62 +2,32 @@ const PROJECTS = [
   {
     day: '1',
     title: 'Expanding Cards',
-    link: '///',
-    image: '///',
+    link: '/1.expandingCards/index.html',
+    image: '/images/1-expanding-cards.png',
   },
   {
     day: '2',
     title: 'Progress Steps',
-    link: '///',
-    image: '///',
+    link: '/2.progressStep/index.html',
+    image: '/images/2-progress-steps.png',
   },
   {
     day: '3',
     title: 'Rotating Navigation Animation',
-    link: '///',
-    image: '///',
+    link: 'http://www.google.com',
+    image: '/images/3-rotating-navigation-animation.png',
   },
   {
     day: '4',
     title: 'Hidden Search Widget',
     link: '///',
-    image: '///',
+    image: '/images/4-hidden-search-widget.png',
   },
   {
     day: '5',
     title: 'Blurry Loading',
     link: '///',
-    image: '///',
-  },
-  {
-    day: '1',
-    title: 'Expanding Cards',
-    link: '///',
-    image: '///',
-  },
-  {
-    day: '2',
-    title: 'Progress Steps',
-    link: '///',
-    image: '///',
-  },
-  {
-    day: '3',
-    title: 'Rotating Navigation Animation',
-    link: '///',
-    image: '///',
-  },
-  {
-    day: '4',
-    title: 'Hidden Search Widget',
-    link: '///',
-    image: '///',
-  },
-  {
-    day: '5',
-    title: 'Blurry Loading',
-    link: '///',
-    image: '///',
+    image: '/images/5-blurry-loading.png',
   },
 ];
 
@@ -65,6 +35,53 @@ class ProjectFactory {
   constructor(parentElement, arrayOfData) {
     this.parentElement = parentElement;
     this.arrayOfProjects = arrayOfData;
+  }
+
+  createProject({ day, title, link, image }) {
+    const projectEL = document.createElement('div');
+    projectEL.classList.add('project');
+
+    const showcaseEL = document.createElement('div');
+    showcaseEL.classList.add('showcase');
+    showcaseEL.style.backgroundImage = `url(${image})`;
+
+    const titleEL = document.createElement('div');
+    titleEL.classList.add('title');
+    titleEL.textContent = title;
+
+    const dayEL = document.createElement('div');
+    dayEL.classList.add('day');
+    dayEL.textContent = 'Day ' + day;
+
+    showcaseEL.insertAdjacentElement('beforeend', titleEL);
+    showcaseEL.insertAdjacentElement('beforeend', dayEL);
+
+    const projectHoverEL = document.createElement('div');
+    projectHoverEL.classList.add('hover');
+    projectHoverEL.insertAdjacentElement('beforeend', titleEL);
+
+    const buttonEL = document.createElement('a');
+    buttonEL.setAttribute('href', link);
+    buttonEL.classList.add('btn');
+    buttonEL.textContent = 'Live Demo';
+    projectHoverEL.insertAdjacentElement('beforeend', buttonEL);
+
+    projectEL.insertAdjacentElement('beforeend', projectHoverEL);
+    projectEL.insertAdjacentElement('beforeend', showcaseEL);
+
+    return projectEL;
+  }
+
+  hoverEffect(elements) {
+    elements.forEach((element) => {
+      element.addEventListener('mouseenter', (e) => {
+        e.target.classList.add('hide');
+      });
+
+      element.addEventListener('mouseleave', (e) => {
+        e.target.classList.remove('hide');
+      });
+    });
   }
 
   startProjectPainting() {
@@ -76,24 +93,8 @@ class ProjectFactory {
     projectsGenerated.forEach((project) =>
       this.parentElement.appendChild(project)
     );
-  }
 
-  createProject({ day, title, link, image }) {
-    const projectEL = document.createElement('div');
-    projectEL.classList.add('project');
-
-    const titleEL = document.createElement('div');
-    titleEL.classList.add('title');
-    titleEL.textContent = title;
-
-    const dayEL = document.createElement('div');
-    dayEL.classList.add('day');
-    dayEL.textContent = day;
-
-    projectEL.appendChild(titleEL);
-    projectEL.appendChild(dayEL);
-
-    return projectEL;
+    this.hoverEffect(projectsGenerated);
   }
 }
 
